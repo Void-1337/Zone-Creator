a1222b2f8d001d26d7431a7d3dcb020122b9aa6d
Shaar <40307680+Shaar-games@users.noreply.github.com>
2019-07-06 20:24:44 +0200

    Add files via upload

--- lua/entities/zone.lua
+++ lua/entities/zone.lua
@@ -1,1 +1,37 @@
 --salut
+
+
+AddCSLuaFile()
+DEFINE_BASECLASS( "base_gmodentity" )
+
+ENT.PrintName = "toolname"
+
+function ENT:SetupDataTables()
+
+
+
+end
+
+function ENT:Initialize()
+
+	if ( CLIENT ) then return end
+
+	print( self )
+
+end
+
+function ENT:GetOverlayText()
+
+	local txt = "salut mec"
+
+	return txt
+
+end
+
+function ENT:OnTakeDamage( dmginfo )
+
+end
+
+function ENT:PhysicsSimulate( phys, deltatime )
+
+end
