ce525b8e234b68a5f3019e80769fdd04fc451044
Shaar-games <40307680+Shaar-games@users.noreply.github.com>
2019-07-07 11:43:59 +0200

    Update zone.lua

--- lua/weapons/gmod_tool/stools/zone.lua
+++ lua/weapons/gmod_tool/stools/zone.lua
@@ -1,17 +1,52 @@
 TOOL.Category = "Zone Creator"
 TOOL.Name = "Zone Creator"
 TOOL.Command = nil
-TOOL.ConfigName = "" --Setting this means that you do not have to create external configuration files to define the layout of the tool config-hud 
- 
-TOOL.ClientConVar[ "myparameter" ] = "fubar"
+TOOL.ConfigName = "Zone Creator" --Setting this means that you do not have to create external configuration files to define the layout of the tool config-hud 
+
+
+TOOL.Information = {
+
+	{ name = "info", stage = 1 },
+	{ name = "left" },
+	{ name = "right" },
+	{ name = "right_use", icon2 = "gui/e.png" },
+	{ name = "reload" },
+	{ name = "reload_use", icon2 = "gui/e.png" },
+
+}
+
+--language.Add( "tool.zone.name", "Zone Creator Tool" )
+--language.Add( "tool.zone.left", "Select an object" )
+--language.Add( "tool.zone.right", "" )
+--language.Add( "tool.zone.reload", "Select yourself" )
+--language.Add( "tool.zone.reload_use", "Hadoken !!!!" )
+--language.Add( "tool.zone.desc", "A simple tool to create zone in the game garry's mod" )
+
  
 function TOOL:LeftClick( trace )
+	--if CLIENT then return end
+	PrintTable( trace )
 end
  
 function TOOL:RightClick( trace )
+	--if CLIENT then return end
+	PrintTable( trace )
+
+	if SERVER then return end
+	render.DrawWireframeBox( trace.HitPos , Angle( 0 , 0 , 0 ) , trace.HitPos , Entity( 1 ):GetEyeTrace().HitPos , Color( 255, 255, 255 ), false )
 end
  
 function TOOL.BuildCPanel( panel )
+
+	--local List = vgui.Create("DPanelList", panel )
+--
+	--for i=1,10 do
+	--	btn = vgui.Create("DButton")
+	--	btn:SetText( i )
+	--	List:AddItem( btn )
+	--end
+
+
 	panel:AddControl("Header", { Text = "Waw", Description = "A simple and easy tool to create custom zone" })
 	
 	if(LocalPlayer():IsAdmin()) then
@@ -25,5 +60,3 @@ function TOOL.BuildCPanel( panel )
 		})
 	end
 end
-
-if SERVER then return end
